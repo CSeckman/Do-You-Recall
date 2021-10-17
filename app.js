0f8a462480f59ef4fa26adecda32ff76f7b494ef
C_Seckman <Cassie.seckman@gmail.com>
2021-10-17 17:23:09 -0400

    add start buttons, add event listeners to start game for each button

--- app.js
+++ app.js
@@ -7,11 +7,18 @@
 
 
 /*------------------------ Cached Element References ------------------------*/
-
+const begButton = document.querySelector('#beginnerBtn')
+const interButton = document.querySelector('#intermediateBtn')
+const expButton = document.querySelector('#expertBtn')
 
 
 /*----------------------------- Event Listeners -----------------------------*/
-
+begButton.addEventListener('click', startGame)
+interButton.addEventListener('click', startGame)
+expButton.addEventListener('click', startGame)
 
 
 /*-------------------------------- Functions --------------------------------*/
+function startGame() {
+  console.log('working click')
+}
