c82d9218784bd57a458e0bd8029c973ba1136488
C_Seckman <Cassie.seckman@gmail.com>
2021-10-20 22:04:05 -0400

    refactor to use modal

--- app.js
+++ app.js
@@ -1,61 +1,46 @@
-/*-------------------------------- Constants --------------------------------*/
 
-
-let inputVal 
-
-/*---------------------------- Variables (state) ----------------------------*/
 const beginnerArray = [1,1,1,1,1]
-const intermediateArray = [1,1,1,1,1,1,1,1,1,1]
-const expertArray = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+const intermediateArray = [1,1,1,1,1,1,1]
+const expertArray = [1,1,1,1,1,1,1,1,1,1]
 let startArray
 let randomArrayLength
-let playerGuess
 let randomArray
-let screenToUpdate
-/*------------------------ Cached Element References ------------------------*/
-const begButton = document.querySelector('#beginnerBtn')
-const interButton = document.querySelector('#intermediateBtn')
-const expButton = document.querySelector('#expertBtn')
-const begScreen = document.querySelector('#beginnerScreen')
-const interScreen = document.querySelector('#intermediateScreen')
-const expScreen = document.querySelector('#expertScreen')
-const begInput = document.querySelector('#begInput')
-const interInput = document.querySelector('#interInput')
-const expInput = document.querySelector('#expInput')
-const submitBtn = document.querySelectorAll('.submitBtn')
-/*----------------------------- Event Listeners -----------------------------*/
+let inputVal 
+
+const begButton = document.querySelector('#beginner-btn')
+const interButton = document.querySelector('#intermediate-btn')
+const expButton = document.querySelector('#expert-btn')
+const screenOfNums = document.querySelector('.screen-of-nums')
+const playerInput = document.querySelector('#player-input')
+const submitBtn = document.querySelector('.submit-btn')
+const resetBtn = document.querySelector('.reset-btn')
+
 begButton.addEventListener('click', startGame)
 interButton.addEventListener('click', startGame)
 expButton.addEventListener('click', startGame)
-submitBtn.forEach(btn => {
-  btn.addEventListener('click', checkForWinner)
-})
-// interInput.addEventListener('click', handleInput)
-// expInput.addEventListener('click', handleInput)
-/*-------------------------------- Functions --------------------------------*/
-// init()
+submitBtn.addEventListener('click', checkForWinner)
+resetBtn.addEventListener('click', closeModal)
 
-// function init() {
-//   begInput.setAttribute('hidden', true)
-//   interInput.setAttribute('hidden', true)
-//   expInput.setAttribute('hidden', true)
-// }
 
 function startGame(evt) {
-  console.log('working click')
-  if (evt.target.id === 'beginnerBtn') {
-    document.getElementById('beginnerScreen').className = "screenOfNums"
+  console.log('hi')
+  document.querySelector('.modal-container').removeAttribute('hidden', true)
+  if (evt.target.id === 'beginner-btn') {
+    document.getElementById('card-title').innerText="Beginner"
+    document.getElementById('number-of-cards').innerText="5 Numbers"
     startArray = beginnerArray
     console.log(startArray)
   }
-  if (evt.target.id === 'intermediateBtn') {
-    document.getElementById('intermediateScreen').className = "screenOfNums"
+  if (evt.target.id === 'intermediate-btn') {
+    document.getElementById('card-title').innerText="Intermediate"
+    document.getElementById('number-of-cards').innerText="7 Numbers"
     startArray = intermediateArray
     console.log(startArray)
 
   }
-  if (evt.target.id === 'expertBtn') {
-    document.getElementById('expertScreen').className = "screenOfNums"
+  if (evt.target.id === 'expert-btn') {
+    document.getElementById('card-title').innerText="Expert"
+    document.getElementById('number-of-cards').innerText="10 Numbers"
     startArray = expertArray
     console.log(startArray)
   }
@@ -72,17 +57,9 @@ function getRandomArray() {
 
 function renderNums (randomArray) {
   randomArrayLength = randomArray.length
-  if (randomArrayLength === 5) {
-    screenToUpdate = begScreen
-  } else if (randomArrayLength === 10) {
-    screenToUpdate = interScreen
-  } else if (randomArrayLength) {
-    screenToUpdate = expScreen
-  }
-  
   randomArray.forEach((num, idx) => {
     setTimeout(()=> {
-      screenToUpdate.innerText = num
+      screenOfNums.innerText = num
       console.log(num)
     }, (idx * 1000))
   })
@@ -90,44 +67,43 @@ function renderNums (randomArray) {
 }
 
 function renderForms() {
+  inputVal = playerInput
   if (randomArrayLength === 5) {
-    begButton.setAttribute('hidden', true)
-    inputVal = begInput
     setTimeout(() => {
-      screenToUpdate.innerText = ''
-      screenToUpdate.className = 'screen'
-      document.getElementById('begForm').removeAttribute('hidden')
+      screenOfNums.innerText = ''
+      screenOfNums.className = 'img'
+      document.getElementById('input-form').removeAttribute('hidden')
     }, 5000)
   }
-  if (randomArrayLength === 10) {
-    interButton.setAttribute('hidden', true)
-    inputVal = interInput
+  if (randomArrayLength === 7) {
     setTimeout(() => {
-      screenToUpdate.innerText = ''
-      screenToUpdate.className = 'screen'
-      document.getElementById('interForm').removeAttribute('hidden')
-    }, 10000)
+      screenOfNums.innerText = ''
+      screenOfNums.className = 'img'
+      document.getElementById('input-form').removeAttribute('hidden')
+    }, 7000)
   }
-  if (randomArrayLength === 15) {
-    expButton.setAttribute('hidden', true)
-    inputVal = expInput
+  if (randomArrayLength === 10) {
     setTimeout(() => {
-      screenToUpdate.innerText = ''
-      screenToUpdate.className = 'screen'
-      document.getElementById('expForm').removeAttribute('hidden')
-    }, 15000)
+      screenOfNums.innerText = ''
+      screenOfNums.className = 'img'
+      document.getElementById('input-form').removeAttribute('hidden')
+    }, 10000)
   }
 }
 
-function checkForWinner () {
+function checkForWinner() {
   console.log("taco")
   console.log(inputVal.value)
   let randomString = randomArray.join('')
   console.log(randomString)
   
   if (inputVal.value === randomString) {
-    screenToUpdate.className = "winningScreen"
-  } else if (inputVal.value !== randomString) {
-    screenToUpdate.className = "tryAgainScreen"
+    screenOfNums.className = "you-win"
+  } else {
+    screenOfNums.className = "try-again"
   }
+}
+
+function closeModal() {
+  document.querySelector('.modal-container').setAttribute('hidden', true)
 }
